81bc514f32b18dbf0b175cf62c64e531111b659b
Sylvain Laporte <laporte.sylvain@outlook.com>
2020-03-10 14:44:16 -0400

    Implement binary search for an array of int

---
+++ java/snippetsjava/src/snippets/algo/search/BinarySearch.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2020 Sylvain Laporte.
+ */
+package snippets.algo.search;
+
+/**
+ * Implementation of binary search.
+ * 
+ * @author sylvainlaporte
+ * @version %I%, %G%
+ * 
+ */
+public class BinarySearch {
+
+	/**
+	 * Finds an element in an array of integers.
+	 * 
+	 * @param array	the array in which to search
+	 * @param value	the value to search
+	 * @return		the index of the first found value; -1 if not found
+	 */
+	public static int binarySearch(int[] array, int value) {
+		int low = 0;
+		int high = array.length;
+		
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			if (array[mid] == value) {
+				return mid;
+			} else if (array[mid] < value) {
+				low = mid + 1;
+			} else if (array[mid] > value) {
+				high = mid - 1;
+			}
+		}
+		return -1;	// Value not found
+	}
+	
+}
